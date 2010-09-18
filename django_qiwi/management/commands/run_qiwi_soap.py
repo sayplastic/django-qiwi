a0a691040713b30fad09307e6c4ed3d859280be6
Ivan Petukhov <satels@gmail.com>
2010-09-18 22:56:46 +0600

    Сделано правильно для команды

--- django_qiwi/management/commands/run_qiwi_soap.py
+++ django_qiwi/management/commands/run_qiwi_soap.py
@@ -1,9 +1,9 @@
 #encoding:utf8
-from django.core.management.base import NoArgsCommand
+from django.core.management.base import BaseCommand
 from django_qiwi.soap.server import Server as QiwiServer
 
 
-class Command(NoArgsCommand):
+class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         server = QiwiServer()
