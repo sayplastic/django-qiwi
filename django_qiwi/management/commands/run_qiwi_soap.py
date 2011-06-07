380fd43a52a6732ded1a736cab94ad8337001585
alexander <alexander@AMD.(none)>
2011-06-08 01:56:19 +0400

    Исправлена команда запуска SOAP сервера

--- django_qiwi/management/commands/run_qiwi_soap.py
+++ django_qiwi/management/commands/run_qiwi_soap.py
@@ -3,7 +3,7 @@ from django.core.management.base import NoArgsCommand
 from django_qiwi.soap.server import runserver
 
 
-class Command(BaseCommand):
+class Command(NoArgsCommand):
 
     def handle_noargs(self, **options):
         runserver()
