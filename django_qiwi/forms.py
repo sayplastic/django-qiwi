a906a160aae216914895d16f2c67ccf9dbfd56a3
Andrey Beletsky <andrey@vr2.net>
2012-12-19 12:34:16 +0400

    form fixes

--- django_qiwi/forms.py
+++ django_qiwi/forms.py
@@ -7,29 +7,32 @@ from django import forms
 from django_qiwi.conf import *
 
 
-class RobokassaForm(BaseRobokassaForm):
-    from = forms.CharField(max_length=20, initial=QIWI_LOGIN)
+class QiwiBillForm(forms.Form):
+    login = forms.CharField(max_length=20, initial=QIWI_LOGIN)
     to = forms.CharField(max_length=20)
     summ = forms.DecimalField()
     com = forms.CharField(max_length=255)
     lifetime = forms.DateTimeField()
     check_agt = forms.IntegerField(initial=QIWI_CREATE)
     txn_id = forms.CharField(max_length=20)
-    currency = forms.IntegerField(max_length=20, initial=QIWI_CURRENCY)
+    currency = forms.IntegerField(initial=QIWI_CURRENCY)
 
     target = QIWI_HTTP_URL
 
     def __init__(self, *args, **kwargs):
-        super(RobokassaForm, self).__init__(*args, **kwargs)
+        super(QiwiBillForm, self).__init__(*args, **kwargs)
         for field in self.fields:
             self.fields[field].widget = forms.HiddenInput()
+        self.fields['from'] = self.fields['login']
+        del self.fields['login']
 
     def get_redirect_url(self):
         def _initial(name, field):
             val = self.initial.get(name, field.initial)
             if not val:
                 return val
-            return unicode(val)#.encode('1251')
+            val = u'{}'.format(val)
+            return val.encode('utf-8')
 
         fields = [(name, _initial(name, field))
                   for name, field in self.fields.items()
